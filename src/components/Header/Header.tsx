f23242180d1cd8fea966276d00e9ea1d87c184ae
Eduardo Ulysséa <eduardoulyssea@MacBook-Air-de-Eduardo.local>
2024-06-05 11:36:03 -0300

    fix a sidebar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -19,6 +19,7 @@ const Header = ({ handleClick }: any) => {
                         <h1 className="text-primaryColor font-bold font-league-spartan py-3">Nek.</h1>
                         <div>
                             {isHeader ? (
+
                                 <div>
                                     <div className="h-screen flex flex-col top-0 right-0 bg-black border-l border-gray-900 z-50 p-2 transition-all px-4 py-4 fixed">
                                         <div className="flex w-full">
@@ -32,16 +33,19 @@ const Header = ({ handleClick }: any) => {
                                                 <Image src={iconAbout} alt="" className='w-5' onClick={() => setIsHeader(!isHeader)} />
                                                 <a href="#about">Sobre nós</a>
                                             </div>
-                                            <div className="flex items-center gap-2 bg-secondaryColor pl-5 py-2 rounded-xl" onClick={() => setIsHeader(!isHeader)}>
-                                                <MdOutlineContactMail /> <p>Entrar em contato </p>
-                                                <span className="relative flex h-3 w-3 right-1 bottom-5">
-                                                    <span className="animate-ping absolute left-2 inline-flex h-full w-full rounded-full bg-primaryColor opacity-75"></span>
-                                                    <span className="relative inline-flex left-2 rounded-full h-3 w-3 bg-primaryColor "></span>
-                                                </span>
+                                            <Link href={'/contact'}>
+                                            <div className="flex items-center gap-2 bg-secondaryColor pl-5 py-2 rounded-xl" >
+                                                <MdOutlineContactMail />   <p>Entrar em contato</p>
+                                                    <span className="relative flex h-3 w-3 right-1 bottom-5">
+                                                        <span className="animate-ping absolute left-2 inline-flex h-full w-full rounded-full bg-primaryColor opacity-75"></span>
+                                                        <span className="relative inline-flex left-2 rounded-full h-3 w-3 bg-primaryColor "></span>
+                                                    </span>
                                             </div>
+                                            </Link>
                                         </div>
-                                        <div className="m-0 p-0 fixed bg-black z-40 w-full inset-0 bg-opacity-70" onClick={() => setIsHeader(!isHeader)}></div>
+                                       
                                     </div>
+                                    <div className="m-0 p-0 fixed bg-black z-40 w-full inset-0 bg-opacity-70" onClick={() => setIsHeader(!isHeader)}></div>
                                 </div>
                             ) : (
                                 <div className="transition-all">
@@ -58,9 +62,9 @@ const Header = ({ handleClick }: any) => {
                         <a href="#servico" className="">Serviços</a>
                         <a href="#about">Sobre nós</a>
                     </div>
-                    
+
                     <Link href={'/contact'}>
-                        <button type="button" className={` bg-primaryColor text-black px-8 rounded-3xl max-md:text-lg `} >
+                        <button type="button" className={` bg-primaryColor text-black px-8 rounded-3xl max-md:text-lg py-2 `} >
                             Contato
                         </button>
                     </Link>
