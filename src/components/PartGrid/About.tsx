bf2d43bf1ce15901a7624cc0ee01b29694b2e6ce
Eduardo Ulysséa <eduardoulyssea@MacBook-Air-de-Eduardo.local>
2024-06-06 23:40:33 -0300

    fix router aand buttons

--- src/components/PartGrid/About.tsx
+++ src/components/PartGrid/About.tsx
@@ -11,7 +11,7 @@ const About = ({aboutRef}:any) => {
     return (
         <div className=' flex bg-secondaryColor rounded-2xl p-4 max-md:p-8 ' ref={aboutRef}>
             <div className='flex pl-8 flex-col justify-center max-md:pl-0 '>
-                <div className='text-3xl font-semibold mb-10 text-primaryColor '>Quem esta por traz da NEK</div>
+                <div className='text-3xl font-semibold mb-10 text-primaryColor '>Quem esta por tras da NEK</div>
                 <div className='w-5/6 text-lg max-md:w-full'>A Nek teve início em janeiro de 2023, fundada por Eduardo Ulysséa. Sua motivação para começar a empresa surgiu da necessidade de dar uma aparência mais profissional aos seus projetos de desenvolvimento de softwares e websites, já que frequentemente contratava diversos freelancers para realizar essas tarefas. Além disso, Eduardo acalentava o sonho de se tornar um empreendedor, e como já possuía experiência como desenvolvedor, viu a oportunidade de unir o útil ao agradável.</div>
             </div>
             {isMobile ? <div> </div> :
